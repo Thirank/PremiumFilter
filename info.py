93fa16f770cb8bd353043c11b812375441d26198
Thirank <116568300+Thirank@users.noreply.github.com>
2023-04-07 19:52:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,6 +69,6 @@ AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'tnlink.in') #Put Only Shortner Site domain don't put like this https://tnlink.in/
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', '') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', '81c24986e7576b9c6d2b7375d2c3e7ed8604dadb')
+SHORTNER_API =  environ.get('SHORTNER_API', '')
